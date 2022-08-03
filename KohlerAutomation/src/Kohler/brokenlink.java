96169ef8f33a3f7b0312c98a4a0fca9321e8cb1a
Yashwanth <110376096+Yashwanth7981@users.noreply.github.com>
2022-08-03 21:13:22 +0530

    Last Commit

--- KohlerAutomation/src/Kohler/brokenlink.java
+++ KohlerAutomation/src/Kohler/brokenlink.java
@@ -47,10 +47,10 @@ public class brokenlink {
 				continue;
 			}
 
-//			if (!url.startsWith(homePage)) {
-//				System.out.println("URL belongs to another domain, skipping it.");
-//				continue;
-//			}
+			if (url.startsWith(homePage)) {
+				System.out.println("URL belongs to another domain, skipping it.");
+				continue;
+			}
 
 			try {
 				huc = (HttpURLConnection) (new URL(url).openConnection());
