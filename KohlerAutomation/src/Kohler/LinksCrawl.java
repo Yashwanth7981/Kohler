9545a7283909bd21b063411c0d53a19ac623b3bb
Yashwanth <110376096+Yashwanth7981@users.noreply.github.com>
2022-09-19 11:45:23 +0530

    NC

--- KohlerAutomation/src/Kohler/LinksCrawl.java
+++ KohlerAutomation/src/Kohler/LinksCrawl.java
@@ -13,7 +13,7 @@ public class LinksCrawl {
 	public static void main(String[] args) throws Throwable {
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		String homePage = "https://youngsoft.in/";
+		String homePage = "http://youngsoft.in/";
 		driver.get(homePage);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
