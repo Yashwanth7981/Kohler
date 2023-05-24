557dd74be3aaf9d4a27892dce5f23f82ac2633df
Yashwanth <110376096+Yashwanth7981@users.noreply.github.com>
2023-05-24 12:44:10 +0530

    Yt com

--- KohlerAutomation/src/Kohler/Youtube.java
+++ KohlerAutomation/src/Kohler/Youtube.java
@@ -15,7 +15,7 @@ public class Youtube {
 		driver.manage().window().maximize();
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//div[@id='search-input']//input")).click();
-		driver.findElement(By.xpath("//div[@id='search-input']//input")).sendKeys("Pitbull on the Floor"+Keys.ENTER);
+		driver.findElement(By.xpath("//div[@id='search-input']//input")).sendKeys("Selenium"+Keys.ENTER);
 		Thread.sleep(5000);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
      	js.executeScript("window.scrollBy(0,100)");	
